f025f29b53dc34c985a18f7262896cf8feefcbac
EmGiGi_UB <mgarcigo536@alumnes.ub.edu>
2026-04-23 13:28:59 +0200

    clase pretec(llarg i normal) terminadas, clase dades faltan un par de metodos

--- src/prog2/model/Dades.java
+++ src/prog2/model/Dades.java
@@ -1,31 +1,72 @@
 package prog2.model;
 
+import prog2.vista.BiblioException;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Dades implements InDades {
+
+    protected ArrayList<Exemplar> llistaExemplars = new ArrayList<>();
+    protected ArrayList<Usuari> llistaUsuaris = new ArrayList<>();
+    protected ArrayList<Prestec> llistaPrestecs = new ArrayList<>();
     /**
      * Afegeix exemplar. Llança excepció si l'id ja existeix
      */
     @Override
-    public void afegirExemplar(String id, String titol, String autor, boolean admetPrestecLlarg) throws BiblioException;
+    public void afegirExemplar(String id, String titol, String autor, boolean admetPrestecLlarg) throws BiblioException {
+        Iterator<Exemplar> iterator = llistaExemplars.iterator();
+
+        while(iterator.hasNext()){
+            Exemplar exemplarActual = iterator.next();
+            if(exemplarActual.getId().equals(id)){
+                throw new BiblioException("Un exemplar amb aquest ID ja està registrat");
+            }
+        }
+        Exemplar(id, autor, titol, admetPrestecLlarg);
+    }
 
     /**
      * Recuperar préstecs. Retorna un ArrayList amb tots els exemplars
      */
     @Override
-    public ArrayList<Exemplar> recuperaExemplars();
+    public ArrayList<Exemplar> recuperaExemplars(){
+        //quiero entender que aquí hay un fallo, es decir, sera recuperar ejemplares
+        Iterator<Prestec> iterator = llistaPrestecs.iterator();
+
+        while(iterator.hasNext()){
+            Prestec prestecActual = iterator.next();
+            prestecActual.retorna();
+        }
+
+        ArrayList<Exemplar> exemplars = new ArrayList<>(llistaExemplars);
+        return exemplars;
+    }
 
     /**
      * Afegeix usuari. Llança excepció si l'email ja existeix
      */
     @Override
-    public void afegirUsuari(String email, String nom, String adreca, boolean esEstudiant) throws BiblioException;
+    public void afegirUsuari(String email, String nom, String adreca, boolean esEstudiant) throws BiblioException{
+        Iterator<Usuari> iterator = llistaUsuaris.iterator();
+
+        while(iterator.hasNext()){
+            Usuari usuariActual = iterator.next();
+            if(usuariActual.getEmail().equals(email)){
+                throw new BiblioException("Un usuari amb aquest email ja està registrat");
+            }
+        }
+        Usuari(email, nom, adreca, esEstudiant);
+    }
 
     /**
      * Recuperar usuaris. Retorna un ArrayList amb tots els usuaris
      */
     @Override
-    public ArrayList<Usuari> recuperaUsuaris();
+    public ArrayList<Usuari> recuperaUsuaris(){
+        ArrayList<Usuari> usuaris = new ArrayList<>(llistaUsuaris);
+        return usuaris;
+    }
 
     /**
      * Afegeix préstec. Ha de fer diferents comprovacions que poden llançar excepcions.
@@ -34,7 +75,9 @@ public class Dades implements InDades {
      */
 
     @Override
-    public void afegirPrestec(int exemplarPos, int usuariPos, boolean esLlarg) throws BiblioException;
+    public void afegirPrestec(int exemplarPos, int usuariPos, boolean esLlarg) throws BiblioException{
+
+    }
 
     /**
      * Retornar préstec. Llança excepció si el prestec ja es vaig retornar.
