002e0f0d3f963ccef886d491008af130f9455326
EmGiGiUB <mgarcigo536@alumnes.ub.edu>
2026-05-09 13:14:59 +0200

    para seguir en ordenador sobremesa

--- src/prog2/adaptador/Adaptador.java
+++ src/prog2/adaptador/Adaptador.java
@@ -1,5 +1,30 @@
 package prog2.adaptador;
+import prog2.model.Dades;
+import prog2.vista.BiblioException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 
 public class Adaptador {
+
+    private Dades dades;
+
+    public void guardaDades(String camiDesti) throws BiblioException{
+        String llistaExemplars = dades.recuperaExemplars().toString();
+        String llistaUsuaris = dades.recuperaUsuaris().toString();
+        String llistaPrestecs = dades.recuperaPrestecs().toString();
+
+        File fitxer = new File(camiDesti);
+        FileOutputStream fout = new FileOutputStream(fitxer);
+        ObjectOutputStream oos = new ObjectOutputStream(fout);
+        oos.writeObject(llistaExemplars);
+
+
+    }
+            ;
+    public void carregaDades(String camiOrigen) throws BiblioException {
+    }
+
 
 }
